bf38aae6ad061cef39624824fb8bc3c08e1ccef7
ArthurCTLin <arthur830119@gmail.com>
2025-08-19 21:06:17 +0800

    CHANGE: requirement.txt to requirements.txt

--- data.py
+++ data.py
@@ -1,16 +1,15 @@
 import os
-import pdfplumber
+import fitz  # PyMuPDF
 
 def load_text(path):
     file_extension = os.path.splitext(path)[1].lower()
 
     if file_extension == ".pdf":
+        doc = fitz.open(path)
         texts = []
-        with pdfplumber.open(path) as pdf:
-            for page in pdf.pages:
-                texts.append(page.extract_text() or "")
-        return "\n".join(texts)
-    
+        for page in doc:
+            texts.append(page.get_text())
+        return "\n".join(texts)    
     elif file_extension == ".txt":
         with open(path, "r", encoding="utf-8") as file:
             return file.read()
